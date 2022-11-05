2641eb7c535be4fb4deda13e16df9561e267d5bf
mattwach <matt.wachowski@gmail.com>
2022-11-04 20:46:33 -0700

    Added map image data

---
+++ examples/midlevel/mapscroll/map.h
@@ -0,0 +1,12 @@
+#ifndef MAP_H
+#define MAP_H
+// Generated data for map.yaml
+
+#define MAP_IMG                        0
+
+#include <inttypes.h>
+#include <pico/platform.h>
+
+extern const uint8_t map[] __in_flash();
+
+#endif  // MAP_H
