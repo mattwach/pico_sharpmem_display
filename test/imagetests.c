a853f51df5946d00e7aa5a2c588934dd2a814ab7
Matt Wachowski <matt.wachowski@gmail.com>
2022-11-15 21:05:37 -0800

    Added more image tests

--- test/imagetests.c
+++ test/imagetests.c
@@ -29,16 +29,16 @@ struct TestData* test_image_imgba(struct Bitmap* bitmap) {
   return &image_imgba_data;
 }
 
-static struct TestData image_imgo1_data = { "imgo1", 12, 0, {} };
-struct TestData* test_image_img01(struct Bitmap* bitmap) {
+static struct TestData image_imgx1_data = { "imgx1", 12, 0, {} };
+struct TestData* test_image_imgx1(struct Bitmap* bitmap) {
   struct BitmapImages bi;
   image_init(&bi, images, bitmap);
   image_draw(&bi, SMALL_IMG, -7, 0);
-  return &image_imgo1_data;
+  return &image_imgx1_data;
 }
 
-static struct TestData image_imgo2_data = {
-  "imgo2",
+static struct TestData image_imgx2_data = {
+  "imgx2",
   2,
   2,
   {
@@ -46,15 +46,15 @@ static struct TestData image_imgo2_data = {
     {0, 4, 1},
   }
 };
-struct TestData* test_image_imgo2(struct Bitmap* bitmap) {
+struct TestData* test_image_imgx2(struct Bitmap* bitmap) {
   struct BitmapImages bi;
   image_init(&bi, images, bitmap);
   image_draw(&bi, SMALL_IMG, -6, 0);
-  return &image_imgo2_data;
+  return &image_imgx2_data;
 }
 
-static struct TestData image_imgo3_data = {
-  "imgo3",
+static struct TestData image_imgx3_data = {
+  "imgx3",
   10,
   10,
   {
@@ -70,17 +70,105 @@ static struct TestData image_imgo3_data = {
     {5, 4, 1},
   }
 };
-struct TestData* test_image_imgo3(struct Bitmap* bitmap) {
+struct TestData* test_image_imgx3(struct Bitmap* bitmap) {
   struct BitmapImages bi;
   image_init(&bi, images, bitmap);
   image_draw(&bi, SMALL_IMG, -1, 0);
-  return &image_imgo3_data;
+  return &image_imgx3_data;
 }
 
-static struct TestData image_imgo4_data = { "imgo4", 12, 0, {} };
-struct TestData* test_image_imgo4(struct Bitmap* bitmap) {
+static struct TestData image_imgx4_data = { "imgx4", 12, 0, {} };
+struct TestData* test_image_imgx4(struct Bitmap* bitmap) {
   struct BitmapImages bi;
   image_init(&bi, images, bitmap);
   image_draw(&bi, SMALL_IMG, bitmap->width - 7, 0);
-  return &image_imgo4_data;
+  return &image_imgx4_data;
+}
+
+static struct TestData image_imgx5_data = { "imgx5", 10, 0, {} };
+struct TestData* test_image_imgx5(struct Bitmap* bitmap) {
+  struct BitmapImages bi;
+  image_init(&bi, images, bitmap);
+  image_draw(&bi, SMALL_IMG, bitmap->width - 6, 0);
+  return &image_imgx5_data;
+}
+
+static struct TestData image_imgx6_data = { "imgx6", 2, 0, {} };
+struct TestData* test_image_imgx6(struct Bitmap* bitmap) {
+  struct BitmapImages bi;
+  image_init(&bi, images, bitmap);
+  image_draw(&bi, SMALL_IMG, bitmap->width - 1, 0);
+  return &image_imgx6_data;
+}
+
+static struct TestData image_imgx7_data = { "imgx7", 0, 0, {} };
+struct TestData* test_image_imgx7(struct Bitmap* bitmap) {
+  struct BitmapImages bi;
+  image_init(&bi, images, bitmap);
+  image_draw(&bi, SMALL_IMG, bitmap->width, 0);
+  return &image_imgx7_data;
+}
+
+static struct TestData image_imgy1_data = { "imgy1", 0, 0, {} };
+struct TestData* test_image_imgy1(struct Bitmap* bitmap) {
+  struct BitmapImages bi;
+  image_init(&bi, images, bitmap);
+  image_draw(&bi, SMALL_IMG, 0, -5);
+  return &image_imgy1_data;
+}
+
+static struct TestData image_imgy2_data = {
+  "imgy2",
+  2,
+  2,
+  {
+    {0, 0, 1},
+    {6, 0, 1},
+  }
+};
+struct TestData* test_image_imgy2(struct Bitmap* bitmap) {
+  struct BitmapImages bi;
+  image_init(&bi, images, bitmap);
+  image_draw(&bi, SMALL_IMG, 0, -4);
+  return &image_imgy2_data;
+}
+
+static struct TestData image_imgy3_data = { "imgy3", 10, 0, {} };
+struct TestData* test_image_imgy3(struct Bitmap* bitmap) {
+  struct BitmapImages bi;
+  image_init(&bi, images, bitmap);
+  image_draw(&bi, SMALL_IMG, 0, -1);
+  return &image_imgy3_data;
+}
+
+static struct TestData image_imgy4_data = { "imgy4", 12, 0, {} };
+struct TestData* test_image_imgy4(struct Bitmap* bitmap) {
+  struct BitmapImages bi;
+  image_init(&bi, images, bitmap);
+  image_draw(&bi, SMALL_IMG, 0, bitmap->height - 6);
+  return &image_imgy4_data;
+}
+
+static struct TestData image_imgy5_data = { "imgy5", 10, 0, {} };
+struct TestData* test_image_imgy5(struct Bitmap* bitmap) {
+  struct BitmapImages bi;
+  image_init(&bi, images, bitmap);
+  image_draw(&bi, SMALL_IMG, 0, bitmap->height - 5);
+  return &image_imgy5_data;
+}
+
+static struct TestData image_imgy6_data = { "imgy6", 2, 0, {} };
+struct TestData* test_image_imgy6(struct Bitmap* bitmap) {
+  struct BitmapImages bi;
+  image_init(&bi, images, bitmap);
+  image_draw(&bi, SMALL_IMG, 0, bitmap->height - 1);
+  return &image_imgy6_data;
+}
+
+static struct TestData image_imgy7_data = { "imgy7", 0, 0, {} };
+struct TestData* test_image_imgy7(struct Bitmap* bitmap) {
+  struct BitmapImages bi;
+  image_init(&bi, images, bitmap);
+  image_draw(&bi, SMALL_IMG, 0, bitmap->height);
+  return &image_imgy7_data;
 }
